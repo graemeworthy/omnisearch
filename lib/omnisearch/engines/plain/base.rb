34e84aeb74e5a25df02e0b16e6384059d4ffc4eb
Graeme Worthy <graemeworthy@gmail.com>
2011-11-29 02:16:11 -0800

    Tests for engines
    And some associated refactoring.
    carrying variables through multiple functions sucks especially when
    they're defaults

--- lib/omnisearch/engines/plain/base.rb
+++ lib/omnisearch/engines/plain/base.rb
@@ -5,6 +5,7 @@ module OmniSearch::Engines
   #
   module Plain
     INDEX = OmniSearch::Indexes::Plain
+    INDEXED_FIELD = :value
   end
   class Plain::Base
     attr_accessor :string
@@ -19,52 +20,41 @@ module OmniSearch::Engines
 
     def initialize(string = "")
       @string = string
-      @top_score = 0
-      @top_hit
     end
 
     def results
-      result = score_index
-      if @top_score > 0
-        result[:top_hit] = [@top_hit]
-      end
-      result
+      score_index
     end
 
     protected
 
     def index
-      @index ||= Plain::INDEX.new.contents
+      Plain::INDEX.new.contents
     end
 
     def score_index
       index.each do |category, items|
-        score_items(items, :value)
+        score_items(items)
         sort_items(items)
+        trim_below_cutoff(items)
       end
     end
 
-    def score_items(items = [], field = 'value')
+    def score_items(items = [])
       items.each do |item|
-         score_item(item, field)
-         top_test(item)
-      end
-      trim_below_cutoff(items)
+         score_item(item)
+      end     
     end
 
-    def score_item(item, field)
-      item_score = score(item[field])
-      item[:score] = item_score
+    def score_item(item)
+      item[:score] = score(item_value(item))
       item
     end
-
-    def top_test(item)
-      return if item[:score] < cutoff
-      if item[:score] >= @top_score
-        @top_score = item[:score]
-        @top_hit = item
-      end
+    
+    def item_value(item)
+      item[Plain::INDEXED_FIELD]
     end
+
 
     def trim_below_cutoff(items)
       items.delete_if {|item| item[:score] < cutoff }
@@ -72,6 +62,7 @@ module OmniSearch::Engines
 
     def sort_items(items = [])
       items.sort!{|a, b| b[:score] <=> a[:score] }
+      
     end
 
   end
--- spec/engines_spec.rb
+++ spec/engines_spec.rb
@@ -1,19 +1,30 @@
 require './spec/spec_helper'
 
+describe "Engines::Plain" do
+let(:the_class) {Engines::Plain}
+describe 'Constants'do
+  it 'INDEX should point at the plain index' do
+    the_class::INDEX.should == OmniSearch::Indexes::Plain
+  end
+end
+
+end
 describe "Engines::Plain::Base" do
 
   let(:the_instance) {Engines::Plain::Base.new}
+  let(:the_class) {Engines::Plain::Base}
+  let(:fake_index) {{
+    :fake_category => [
+      {:id => 1, :value => 'puppies'},
+      {:id => 2, :value => 'kittens'},
+      {:id => 3, :value => 'apples'},
+      {:id => 4, :value => 'oranges'},
+      ]
+   }}
+ 
   describe 'The Big Picture' do
-    it "scores all items in an index, dropping those below cutoff, highlighting the top ones" do
-      the_instance.stub(:index) {{
-
-        :fake_index => [
-          {:id => 1, :value => 'puppies'},
-          {:id => 2, :value => 'kittens'},
-          {:id => 3, :value => 'apples'},
-          {:id => 4, :value => 'oranges'},
-          ]
-       }}
+    it "scores all items in an index, dropping those below cutoff" do
+      the_instance.stub(:index) {fake_index}
 
        the_instance.stub(:cutoff) {3}
        counter = 0
@@ -23,68 +34,109 @@ describe "Engines::Plain::Base" do
        }
 
        the_instance.results.should == {
-        :fake_index=>
+        :fake_category=>
           [
             {:id=>4, :value=>"oranges", :score=>4},
             {:id=>3, :value=>"apples", :score=>3}
-          ],
-        :top_hit=>
-          [
-            {:id=>4, :value=>"oranges", :score=>4}
           ]
         }
 
     end
   end
-  describe 'Constants'do
-    it 'INDEX should point at an Index'
-  end
   describe 'Interfaces' do
-    it '#score should raise notImplemented'
-    it '#cutoff should raise notImplemented'
+    
+    it '#score should raise notImplemented' do
+     expect { the_instance.score('') }.to raise_error
+    end
+    it '#cutoff should raise notImplemented' do
+      expect { the_instance.cutoff }.to raise_error
+      
+    end    
   end
   describe 'Constructor' do
-    it '@string is the argument'
-    it '@top_score, begins at 0'
-    it '@top_hit begins nil'
+    it '@string is the argument' do
+      argument = 'some string'
+      an_instance = the_class.new(argument)
+      an_instance.string.should == argument
+    end
   end
   describe 'results' do
-    it 'returns the scored_index'
-    it "includes the top hit, if there is one"
-    it "doesn't include the top_hit if there isn't one"
+    it 'returns the scored_index' do
+      the_instance.should_receive(:score_index)
+      the_instance.results      
+    end
   end
   describe 'index' do
-    it 'returns the contents of of INDEX'
-    it 'memoizes the results'
+    it 'returns the contents of of INDEX' do
+      dummy_index = double()
+      OmniSearch::Indexes::Plain.should_receive(:new).and_return(dummy_index)
+      dummy_index.should_receive(:contents).and_return({})
+      the_instance.send(:index)
+    end
   end
-  describe 'score_index' do
-    it 'scores the contents of each category'
-    it 'sorts the contents of each category'
-  end
-  describe 'score_items' do
-    it 'has a terrible name'
-    it 'takes an array as an argument'
-    it 'takes a  field as an argument, optionaly'
-    it 'calls score_hash on each item'
-    it 'calls top_test   on each item'
-    it 'calls trim_below_cutoff, on the whole set'
-  end
-  describe 'score_item' do
-    # this requires score_item to be defined
-    it 'sets :score, to be the result of score on item[field]'
-  end
-  describe 'top_test' do
-    it 'aborts if item[:score] below cutoff'
-    it "bumps the top_hit, if it's better"
-    it "bumps the top_hit, if it's equal"
-    it "doesn't bump if it's not better"
-  end
-  describe 'trim below cutoff' do
-    #this requires cutoff to be defined
-    it 'removes all items whose scores are below cutoff'
-  end
-  describe 'sort_items' do
-    it 'sorts all items by score'
+  describe 'the scoring' do
+    before(:each) do
+      the_instance.stub(:index) {fake_index}
+      the_instance.stub(:cutoff) {0}      
+      the_instance.stub(:score) {1}    
+    end
+    
+    describe 'score_index' do
+      it 'scores, sorts and trims the contents of each category' do
+        the_instance.should_receive(:score_items)
+        the_instance.should_receive(:sort_items)
+        the_instance.should_receive(:trim_below_cutoff)
+        the_instance.send(:score_index)
+      end
+    end
+
+    describe 'score_items' do
+      it 'takes an array as an argument' do
+        the_instance.send(:score_items, [])      
+      end
+
+      it 'calls score_item on each item' do
+        one = double()
+        two = double()
+        items = [one, two]
+        the_instance.should_receive(:score_item).with(one)
+        the_instance.should_receive(:score_item).with(two)
+        the_instance.send(:score_items, items)      
+      end
+     
+    end
+    describe 'score_item' do
+      # this requires score_item to be defined
+      it 'sets :score, to be the result of score on item[field]' do 
+          item = {:id => 1, :value => 'pants'}        
+          the_instance.send(:score_item, item)
+          item.keys.include?(:score).should == true
+      end
+    end
+    describe 'trim below cutoff' do
+      it 'removes all items whose scores are below cutoff' do
+        # down with pants, up with skirts!
+        below = {:id => 1, :value => 'pants', :score => -1}
+        above = {:id => 2, :value => 'skirts', :score => 1}
+        items = [below, above]
+        the_instance.send(:trim_below_cutoff, items)
+        
+        items.should == [above]        
+      end
+    end
+    describe 'sort_items' do
+      it 'sorts all items by score' do
+        bottom = {:id => 3, :value => 'pancakes', :score =>  1}
+        middle = {:id => 1, :value => 'waffles', :score => 2}
+        top    = {:id => 2, :value => "waffles with icecream", :score => 3}
+        items = [middle, top, bottom]
+        the_instance.send(:sort_items, items)
+        items.should ==  [top, middle, bottom]
+        items.should_not == [middle, bottom, top]
+        
+
+      end
+    end
   end
 end
 
