fb6dbcd290127a76f2cf7134ddf19efdc1535efa
Sebastian Friedrich <sebastian@feldpost.com>
2011-12-10 10:34:34 -0800

    Rename img to thumbnail

--- lib/omni_search/result.rb
+++ lib/omni_search/result.rb
@@ -3,20 +3,19 @@ module OmniSearch
   class Result
     attr_accessor :id
     attr_accessor :value
-    attr_accessor :img
+    attr_accessor :thumbnail
     attr_accessor :klass
     attr_accessor :raw_data
     attr_accessor :score
 
     alias_method :label, :value
-
     def initialize(data={})
-      @raw_data = data
-      @score = data[:score]
-      @value = data[:label]
-      @id    = data[:id]
-      @img   = data[:img]
-      @klass = data[:klass]
+      @raw_data   = data
+      @score      = data[:score]
+      @value      = data[:label]
+      @id         = data[:id]
+      @thumbnail  = data[:thumbnail]
+      @klass      = data[:klass]
     end
 
   end
--- spec/result_spec.rb
+++ spec/result_spec.rb
@@ -4,32 +4,32 @@ describe Result do
   let(:the_class) {Result}
   subject { Result.new(:label => "A result", :score => 1) }
   describe "Instance Methods" do
-   it {should respond_to :id}
-   it {should respond_to :klass}
-   it {should respond_to :img}
-   it {should respond_to :raw_data}
-   it {should respond_to :score}
-   it {should respond_to :value}
+    it {should respond_to :id}
+    it {should respond_to :klass}
+    it {should respond_to :thumbnail}
+    it {should respond_to :raw_data}
+    it {should respond_to :score}
+    it {should respond_to :value}
   end
   it 'takes a hash as its argument' do
     the_class.new({}).should be
   end
   it 'exposes a lot of passed arguments' do
-    the_class.new({:id => 'this'}).id.should == 'this'
-    the_class.new({:klass => 'this'}).klass.should == 'this'
-    the_class.new({:img => 'this'}).img.should == 'this'
-    the_class.new({:score => 'this'}).score.should == 'this'
+    the_class.new({:id => 'this'}).id.should eq 'this'
+    the_class.new({:klass => 'this'}).klass.should eq 'this'
+    the_class.new({:thumbnail => 'this'}).thumbnail.should eq 'this'
+    the_class.new({:score => 'this'}).score.should eq 'this'
   end
   it 'exposes hash[:label] as value, on purpose' do
     the_class.new({:label => 'this', :value => 'that'}).value.
-       should == 'this'
+    should eq 'this'
     the_class.new({:label => 'this', :value => 'that'}).label.
-       should == 'this'
-    
+    should eq 'this'
+
   end
 
   it 'should have a label' do
-    subject.label.should == "A result"
+    subject.label.should eq "A result"
   end
 
 end
