2d25d34f854b56ea7f956e113dee11c0e71d8a21
Graeme Worthy <graeme@workben.ch>
2013-02-08 13:25:42 -0800

    remove puts statements for timing searches

--- lib/omni_search/search_strategy.rb
+++ lib/omni_search/search_strategy.rb
@@ -3,30 +3,11 @@ module OmniSearch
   class Search::Strategy
 
     def self.run(term)
-      a = Time.now
       results = ResultSet::Factory.sets(Indexes::Plaintext, Engines::Regex, term, 0)
-      b = Time.now
-      puts "omnisearch regex: #{b-a}"
-      if results == []
-        a = Time.now
-        results = ResultSet::Factory.sets(Indexes::Plaintext, Engines::StringDistance, term, 0.55)
-        b = Time.now
-        puts "omnisearch stringdistance: #{b-a}"
-        # a = Time.now
-        # results = ResultSet::Factory.sets(Indexes::Plaintext, Engines::StartDistance, term, 0.55)
-        # b = Time.now
-        puts "omnisearch startdistance: #{b-a}"
-
-      end
-      # if results == []
-      #   a = Time.now
-      #   results = ResultSet::Factory.sets(Indexes::Trigram, Engines::Triscore, term, 0.2)
-      #   b = Time.now
-      #   puts "omnisearch trigram: #{b-a}"
-      # end
-
-      results
+      return results unless results.empty?
+      #failover
+      return ResultSet::Factory.sets(Indexes::Plaintext, Engines::StringDistance, term, 0.55)
     end
 
   end
-end  
+end
